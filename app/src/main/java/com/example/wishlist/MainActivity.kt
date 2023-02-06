a390f1e390da8b59fad372bcba6fb60dea35eacb
cae26 <cea26@njit.edu>
2023-02-06 00:06:31 -0500

    adapter

--- app/src/main/java/com/example/wishlist/MainActivity.kt
+++ app/src/main/java/com/example/wishlist/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.example.wishlist
 
+import WishListAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.widget.Button
+import android.widget.EditText
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private val wishlists : MutableList<Item> = ArrayList()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val wishListRv = findViewById<RecyclerView>(R.id.recyclerView)
+
+        val adapter = WishListAdapter(wishlists)
+
+        val submitButton = findViewById<Button>(R.id.submit)
+        val itemNameText = findViewById<EditText>(R.id.itemName)
+        val priceText = findViewById<EditText>(R.id.price)
+        val webAddressText = findViewById<EditText>(R.id.webAddress)
+
+        wishListRv.adapter =adapter
+
+        wishListRv.layoutManager = LinearLayoutManager(this)
+
+
+        submitButton.setOnClickListener {
+            wishlists.add(Item(itemNameText.text.toString(),priceText.text.toString(),webAddressText.text.toString()))
+
+            itemNameText.onEditorAction(EditorInfo.IME_ACTION_DONE)
+            priceText.onEditorAction(EditorInfo.IME_ACTION_DONE)
+            webAddressText.onEditorAction(EditorInfo.IME_ACTION_DONE)
+            itemNameText.text = null
+            priceText.text = null
+            webAddressText.text = null
+
+            adapter.notifyDataSetChanged()
+
+        }
+            //.setText(null)
     }
 }
---
+++ app/src/main/java/com/example/wishlist/WishListAdapter.kt
@@ -0,0 +1,62 @@
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.wishlist.Item
+import com.example.wishlist.R
+
+class WishListAdapter (private val items: List<Item>) : RecyclerView.Adapter<WishListAdapter.ViewHolder>() {
+
+
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        val itemTextView: TextView
+        val urlTextView: TextView
+        val priceTextView: TextView
+
+        init {
+            // TODO: Store each of the layout's views into
+            // the public final member variables created above
+            itemTextView = itemView.findViewById(R.id.itemText)
+            urlTextView = itemView.findViewById(R.id.urlText)
+            priceTextView = itemView.findViewById(R.id.priceText)
+        }
+
+
+
+    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+        // Inflate the custom layout
+        val contactView = inflater.inflate(R.layout.wish_list_item, parent, false)
+        // Return a new holder instance
+        return ViewHolder(contactView)
+    }
+
+
+    override fun getItemCount(): Int {
+       // TODO("Not yet implemented")
+        return items.size
+    }
+
+
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        // Get the data model based on position
+        val item = items.get(position)
+        // Set item views based on views and data model
+        holder.itemTextView.text = item.itemName
+        holder.urlTextView.text = item.webLink
+        holder.priceTextView.text = item.price
+    }
+
+
+
+
+
+
+}
